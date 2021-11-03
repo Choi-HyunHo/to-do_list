c9efa17b9837f21741eb23b2e11db02efbf38b5a
최현호 <x7788@naver.com>
2021-11-03 18:51:33 +0900

    form hidden 및 데이터 보내기

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDDEN_CLASSNAME = 'hidden'
 
 function onLoginSubmit(event) {
   event.preventDefault() // 브라우저의 기본 동작을 막아준다.
-  console.log(loginInput.value)
+  loginForm.classList.add(HIDDEN_CLASSNAME) // 값을 입력받으면 form을 사라지게 한다.
+  const username = loginInput.value // user의 이름을 변수(username)로 저장해준다.
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
