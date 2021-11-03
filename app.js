a67c8ef7b2c4b056a3c22be7b63a007a145468e4
최현호 <x7788@naver.com>
2021-11-03 19:15:32 +0900

    localStorage 추가

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault() // 브라우저의 기본 동작을 막아준다.
   loginForm.classList.add(HIDDEN_CLASSNAME) // 값을 입력받으면 form을 사라지게 한다.
   const username = loginInput.value // user의 이름을 변수(username)로 저장해준다.
+  localStorage.setItem('username', username) // key: 저장될 값의 이름, value : 변수(값)
   greeting.innerText = `Hello ${username}`
   greeting.classList.remove(HIDDEN_CLASSNAME)
 }
