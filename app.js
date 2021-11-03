3fb3753f6865d9d6d3f61ea3612fafd8a2b35bf3
최현호 <x7788@naver.com>
2021-11-03 17:25:18 +0900

    Login 기능 구현 중 preventDefault 사용

---
+++ app.js
@@ -0,0 +1,9 @@
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+
+function onLoginSubmit(event) {
+  event.preventDefault() // 브라우저의 기본 동작을 막아준다.
+  console.log(loginInput.value)
+}
+
+loginForm.addEventListener('submit', onLoginSubmit)
