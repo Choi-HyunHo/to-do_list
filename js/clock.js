c8b171bac93ffa2af79a222164d13dca86dd97df
최현호 <x7788@naver.com>
2021-11-04 19:03:00 +0900

    시계 완성

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,11 @@ const clock = document.querySelector('#clock')
 
 function getClock() {
   const date = new Date() // 새로운 object 생성
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const hours = String(date.getHours()).padStart(2, '0') // getHours() number 타입 -> string 변환
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+  const seconds = String(date.getSeconds()).padStart(2, '0')
+  clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
-getClock() // 1초 delay가 생기는 것을 방지하기 위해 한 번 호출한다.
+getClock() // 초기에 1초 delay가 생기는 것을 방지하기 위해 한 번 호출한다.
 setInterval(getClock, 1000)
