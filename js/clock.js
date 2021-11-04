bbea2211f58dd9a4efc53cb2314498d39ab29684
최현호 <x7788@naver.com>
2021-11-04 18:20:23 +0900

    date를 사용해서 시계 만들기 part.1

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector('#clock')
 
-function sayHello() {
-  console.log('hello')
+function getClock() {
+  const date = new Date() // 새로운 object 생성
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
 
-setInterval(sayHello, 5000)
+getClock() // 1초 delay가 생기는 것을 방지하기 위해 한 번 호출한다.
+setInterval(getClock, 1000)
